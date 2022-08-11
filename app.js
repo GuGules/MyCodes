e7a3d473fc598daee3ce530035fc1588710dd615
GuGules <89942177+GuGules@users.noreply.github.com>
2022-08-11 21:40:20 +0200

    Final

---
+++ app.js
@@ -0,0 +1,13 @@
+let os_menu=document.querySelector("#menu");
+let close_button=document.querySelector("#close-button")
+function avert() {
+    alert("Ce site est optimisé pour être consulté en 1080p");
+};
+
+function select_os(){
+    os_menu.classList.remove("hidden");
+};
+
+close_button.addEventListener("click",()=>{
+    os_menu.classList.add("hidden")
+})
