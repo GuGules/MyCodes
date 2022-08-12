a487de2e127be09492d57add9dce8bf6b54943f1
GuGules <89942177+GuGules@users.noreply.github.com>
2022-08-13 00:25:32 +0200

    daz

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let os_menu=document.querySelector("#menu");
-let close_button=document.querySelector("#close-button")
+let close_button=document.querySelector("#close-button");
 function avert() {
     alert("Ce site est optimisé pour être consulté en 1080p");
 };
@@ -9,5 +9,9 @@ function select_os(){
 };
 
 close_button.addEventListener("click",()=>{
-    os_menu.classList.add("hidden")
+    os_menu.classList.add("hidden");
 })
+
+function offline(){
+    alert("Le site n'est plus en ligne");
+}
